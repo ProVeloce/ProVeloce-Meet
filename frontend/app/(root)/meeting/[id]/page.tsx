a15bd11262e8baeb72a202a20052d974778b3a85
ProVeloce <developers@proveloce.com>
2025-11-18 23:10:47 +0530

    Updated page.tsx

--- frontend/app/(root)/meeting/[id]/page.tsx
+++ frontend/app/(root)/meeting/[id]/page.tsx
@@ -25,10 +25,9 @@ const MeetingPage = () => {
     </p>
   );
 
-  // get more info about custom call type:  https://getstream.io/video/docs/react/guides/configuring-call-types/
-  const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((m) => m.user.id === user.id));
-
-  if (notAllowed) return <Alert title="You are not allowed to join this meeting" />;
+  // Access control is handled by the backend and Meeting model
+  // Stream.io calls are accessible if the user has the meeting link
+  // Additional access checks can be added here if needed based on call.state.members
 
   return (
     <main className="h-screen w-full">
